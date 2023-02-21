2e7270432ea92db936b3c49bd73e861be24de0a0
jeeknowme <cyrusloniego2@gmail.com>
2023-02-21 23:45:57 +0800

    Added Database configurations

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,17 +6,18 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
 
     def test_add_numbers(self):
         """Test adding the numbers together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEquals(res,11)
+        self.assertEquals(res, 11)
 
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
-        self.assertEquals(res,5)
+        self.assertEquals(res, 5)
